ecab894c527285d0bf58b495baab736b7c30323b
Weijie Xu <weijiexu@amazon.com>
2024-03-14 19:48:44 +0000

    Finish editing extractive QA results

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ Be sure to:
 * Change the title in this README
 * Edit your repository description on GitHub
 
+## Benchmark Performance on Extractive QA
+| F1    | Exact Match | BLEU    | Method                        |
+|-------|-------------|---------|-------------------------------|
+|0.045  | 0.000       | 0.011   | bert-base-uncased             |
+|0.047  | 0.000       | 0.010   | distilbert-base-uncased       |
+|0.642  | 0.000       | 0.140   | roberta-base-squad2           |
+|0.050  | 0.000       | 0.011   | albert                        |
+|0.050  | 0.001       | 0.011   | electra-small-discriminator   |
+|0.072  | 0.000       | 0.020   | xlnet-base                    |
+|0.710  | 0.000       | 0.217   | deberta-v3-large              |
+|0.588  | 0.000       | 0.134   | mdeberta-v3-base              |
+|0.721  | 0.519       | 0.168   | distilbert-base               |
+|0.786  | 0.598       | 0.174   | bert-large-uncased            |
+
 ## Security
 
 See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
--- leaderboard/qa_benchmark_evaluation.py
+++ leaderboard/qa_benchmark_evaluation.py
@@ -1,1 +1,24 @@
 from metric import calculate_f1_score, calculate_exact_match, calculate_bleu, calculate_rouge, calculate_meteor
+import pandas as pd
+evaluations = pd.read_csv('qa_dataset_result.csv').fillna('')
+methods = ['bert-base-uncased_result', 'distilbert-base-uncased_result',
+       'roberta-base-squad2_result', 'albert-base-v2_result',
+       'electra-small-discriminator_result', 'xlnet-base-cased_result',
+       'deberta-v3-large-squad2_result', 'mdeberta-v3-base-squad2_result',
+       'distilbert-base-cased-distilled-squad_result',
+       'bert-large-uncased-whole-word-masking-finetuned-squad_result']
+results = []
+for method in methods:
+    result = []
+    result.append(calculate_f1_score(evaluations['answer'], evaluations[method]))
+    result.append(calculate_exact_match(evaluations['answer'].tolist(), evaluations[method].tolist()))
+    result.append(calculate_bleu(evaluations['answer'], evaluations[method]))
+    
+    results.append(result)
+eva = pd.DataFrame(results, columns = ['F1', 'Exact Match', 'BLEU'])
+eva['method'] = ['bert-base-uncased', 'distilbert-base-uncased', 'roberta-base-squad2', 'albert', 
+                 'electra-small-discriminator', 'xlnet-base', 'deberta-v3-large','mdeberta-v3-base',
+                  'distilbert-base',  'bert-large-uncased']
+eva.to_csv('qa_evaluation.csv')
+
+
